50a71f2bd6447129697d54669d5965a11111c61f
Ahmad Reza Enshaee <enshaee.ahmadreza@gmail.com>
2022-02-22 00:20:15 +0330

    added main and dependencies.

---
+++ go.mod
@@ -0,0 +1,10 @@
+module sika.org/jwtsample
+
+go 1.17
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.10.4
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292
+)
---
+++ src/jwt-sample/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
+	"github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
+)
+
+
+func main() {
+
+}
